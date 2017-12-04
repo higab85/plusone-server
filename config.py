21b4b93641a05e4b91b2e0244b760c88a05739c6
higab85 <gabrielhirschbaeck@gmail.com>
2017-12-05 00:01:56 +0100

    added heroku db(postgres)

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 DEBUG = True
 SECRET_KEY = "mysecret"
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@localhost/plusone'
+SQLALCHEMY_DATABASE_URI = 'postgres://foqoathejkauyl:5d1e146269753b67cc836fdf89afa29c65d52ac69c9add560dda7cfb1fd8586f@ec2-54-235-219-113.compute-1.amazonaws.com:5432/dd3n2pe57rc68c'
