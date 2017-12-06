559ea085991466d8d36de98be3c839f6ea81770b
higab85 <gabrielhirschbaeck@gmail.com>
2017-12-06 15:04:58 +0100

    debug

--- event.py
+++ event.py
@@ -47,8 +47,8 @@ def search_events(current_user):
         'longitude': Event.longitude
     }
 
-    print("args: %s" args)
-    events = Event.query.all()
+    print("args: %s", args)
+    events = Event.query
     for (key,value) in args:
         print("filter: %s == %s\n", attributes[key], value)
         events.filter(attributes[key] == value)
